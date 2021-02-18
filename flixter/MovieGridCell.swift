9a5de40afb991b9b4b9a20f58ce42fdd801e8d50
ander9991 <60303192+ander9991@users.noreply.github.com>
2021-02-18 17:25:29 -0500

    initial commit

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Steven on 2/18/2021.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
